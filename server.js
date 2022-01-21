ad4a0fd112c5b93903b09792b3944342380b70fc
Marcus Ikegami <hikegami.6@gmail.com>
2022-01-21 12:54:00 -0700

    Directories and basic Express server files initialized

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
